d8e7b7c7519f3b13f86c178960754b6acbfef363
mycelion <115478079+mycelion@users.noreply.github.com>
2025-06-16 18:16:18 +0200

    Create 4_sessionState.py

---
+++ streamlit/4_sessionState.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+# Session_state sluzy do zapisywania danych
+# miedzy ponownymy renderami aplikacji
+
+# Inicjalizacja wartości w session_state
+if "count" not in st.session_state:
+    st.session_state.count = 0
+
+# Funkcja zwiększająca licznik
+def increment_counter():
+    st.session_state.count += 1
+
+st.title("Przykład użycia st.session_state")
+
+# Przycisk do zwiększenia licznika
+st.button("Kliknij mnie!", on_click=increment_counter)
+
+# Wyświetlenie aktualnej wartości licznika
+st.write(f"Licznik: {st.session_state.count}")
